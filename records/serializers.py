98ee318a5dfba3f9a23b388c895987e1369356f4
Lachlan Musicman <datakid@gmail.com>
2014-10-26 14:47:12 +1100

    Added in the long form of each data type

--- records/serializers.py
+++ records/serializers.py
@@ -1,20 +1,29 @@
-from records.models import Patient, Seizure, Medication, Surgery
+from records.models import Patient, Seizure, Medication, Surgery, SURGERY_CHOICES
 from rest_framework import serializers
 
 class SeizureSerializer(serializers.ModelSerializer):
+    long_severity = serializers.CharField(source='get_episode_severity_display')
+    long_frequency = serializers.CharField(source='get_frequency_display')
+    long_confidence = serializers.CharField(source='get_event_confidence_display')
+ 
     class Meta:
         model = Seizure
-        fields = ('assessment_date', 'frequency', 'episode_severity', 'event_confidence' )
+        fields = ('assessment_date', 'frequency', 'long_frequency', 'episode_severity', 'long_severity', 'event_confidence', 'long_confidence')
 
 class MedSerializer(serializers.ModelSerializer):
+    long_dose = serializers.CharField(source='get_dose_unit_display')
+    long_frequency = serializers.CharField(source='get_frequency_display')
+ 
     class Meta:
         model = Medication
-        fields = ('dosage', 'dose_unit', 'frequency', 'name', 'date', 'no_medications')
+        fields = ('dosage', 'dose_unit', 'long_dose', 'frequency', 'long_frequency', 'name', 'date', 'no_medications')
 
 class SurgerySerializer(serializers.ModelSerializer):
+    long_type = serializers.CharField(source='get_surgery_type_display')
+
     class Meta:
         model = Surgery
-        fields = ('date', 'surgery_type')
+        fields = ('date', 'surgery_type', 'long_type')
 
 class PatientSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
